05964c79f847e2b2e5b318fd73434dcaad50d4b1
Jay <156947257+jaysal142@users.noreply.github.com>
2026-03-29 21:44:54 -0600

    INITIAL COMMIT
    This is the first commit for the web game final project. The current build is my test build/ proof of concept. As of now the main components are as follows: HTML main game window, game logic vis JavaScript and canvas rendering, and CSS for styiling.
    
    Next steps will be to begin alpha build 0.0.1 with back-end database structure development persisting parallel to game and main-page development.
    
    21:44 - 3.29.2026
    Salgado, J

---
+++ camera.js
@@ -0,0 +1,236 @@
+const root = document.getElementById("game-root");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+const reset = document.getElementById("reset");
+const skyImg = new Image();
+const bgImg = new Image();
+const mgImg = new Image();
+const fgImg = new Image();
+const cbgImg = new Image();
+const cbaseImg = new Image();
+const cbarrelImg = new Image();
+
+skyImg.src = "images/city 8/1.png";
+bgImg.src = "images/city 8/3.png";
+mgImg.src = "images/city 8/4.png";
+fgImg.src = "images/city 8/5.png";
+cbgImg.src = "images/project_window.png";
+
+canvas.width = root.getBoundingClientRect().width;
+canvas.height = 400;
+
+const worldWidth = 3000;
+const groundY = 350;
+
+const gravity = 980;
+
+const projectile = {
+    x: 100,
+    y: groundY,
+    vx: 0,
+    vy: 0,
+    radius: 10,
+    launched: false
+};
+
+const cannon = {
+    x: 100,
+    y: groundY,
+    angle: 45,
+    minAngle: 10,
+    maxAngle: 80,
+    power: 0,
+    maxPower: 1200,
+    chargeSpeed: 1000,
+    charging: false
+}
+
+const cCosmetics = {
+    barrelImage: "images/cannons/starter_barrel.png",
+    baseImage: "images/cannons/starter_base.png"
+}
+
+const camera = {
+    x: 0,
+    y: 0
+};
+
+function fireCannon() {
+    const angleRad = cannon.angle * Math.PI / 180;
+
+    projectile.x = cannon.x;
+    projectile.y = cannon.y;
+
+    projectile.vx = cannon.power * Math.cos(angleRad);
+    projectile.vy = -cannon.power * Math.sin(angleRad);
+
+    projectile.launched = true;
+}
+
+function update(dt) {
+    cbaseImg.src = cCosmetics.baseImage;
+    cbarrelImg.src = cCosmetics.barrelImage;
+    canvas.width = root.getBoundingClientRect().width;
+
+    if (cannon.charging) {
+        cannon.power += cannon.chargeSpeed * dt;
+        cannon.power = Math.min(cannon.power, cannon.maxPower);
+    }
+
+    if (projectile.launched) {
+        projectile.vy += gravity * dt;
+        projectile.x += projectile.vx * dt;
+        projectile.y += projectile.vy * dt;
+
+        if (projectile.y > groundY) {
+            projectile.y = groundY;
+            projectile.vy = 0;
+            projectile.vx = 0;
+            projectile.launched = false;
+            let score = projectile.x - cannon.x;
+            document.getElementById("w-score").style.visibility = 'visible';
+            document.getElementById("score").textContent += score.toFixed(2);
+        }
+    }
+    // HORIZONTAL
+    camera.x = projectile.x - canvas.width / 6;
+    // VERTICAL
+    const halfScreen = canvas.height / 2;
+
+    if (projectile.y > groundY - halfScreen) {
+        camera.y += (0 - camera.y) * 4 * dt;
+    } else {
+        const targetY = projectile.y - halfScreen;
+        const followStrength = projectile.vy < 0 ? 5 : 2;
+        camera.y += (targetY - camera.y) * followStrength * dt;
+    }
+}
+
+function drawInfLayer(img, speed, vertFactor, fillColor) {
+    if (!img.complete) return;
+
+    const layerWidth = img.width;
+    const offsetX = (camera.x * speed) % layerWidth;
+    const baseY = Math.floor(groundY - img.height);
+
+    ctx.save();
+    ctx.translate(-offsetX, -camera.y * vertFactor);
+
+    for (let x = -layerWidth; x < canvas.width + layerWidth; x += layerWidth) {
+        ctx.drawImage(img, x, baseY);
+        ctx.fillStyle = fillColor;
+        ctx.fillRect(x, baseY + img.height, layerWidth, canvas.height * 2);
+    }
+    ctx.restore();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // SKY
+    drawInfLayer(skyImg, 0, 0, "#5b9da5");
+    // BACKGROUND
+    drawInfLayer(bgImg, 0.2, 0.3, "#5d3a6a");
+    // MIDGROUND
+    drawInfLayer(mgImg, 0.5, 0.6, "#21151c");
+    // FOREGROUND
+    drawInfLayer(fgImg, 0.8, 0.9, "#2e294e");
+    
+    let power = (cannon.power / cannon.maxPower) * 200;
+    document.documentElement.style.setProperty('--power', power + 'px');
+
+    ctx.save();
+    ctx.translate(-camera.x, -camera.y);
+
+    // GROUND
+    ctx.fillStyle = "#655776";
+    ctx.fillRect(camera.x - canvas.width, groundY, canvas.width * 3, canvas.height);
+
+    ctx.drawImage(cbgImg, -50, cannon.y / 2 , 300, 300 * cbgImg.height / cbgImg.width);
+
+    // BALL
+    ctx.beginPath();
+    ctx.arc(projectile.x, projectile.y, projectile.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#22e622";
+    ctx.fill();
+
+    // CANNON
+    ctx.save();
+    ctx.translate(cannon.x, cannon.y);
+    ctx.rotate(-cannon.angle * Math.PI / 180);
+    ctx.fillStyle = "#dee0df";
+    ctx.drawImage(cbarrelImg, -20, -80, 150, 150);
+    ctx.restore();
+    ctx.drawImage(cbaseImg, cannon.x / 3 - 10, cannon.y - 80, 150, 150 * cbaseImg.height / cbaseImg.width);
+}
+
+let lastTime = 0;
+function loop(timestamp) {
+    const dt = (timestamp - lastTime) / 1000;
+    lastTime = timestamp;
+
+    update(dt);
+    draw();
+
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp" || e.key === "w") {
+        cannon.angle = Math.min(cannon.angle + 2, cannon.maxAngle);
+    }
+    if (e.key === "ArrowDown" || e.key === "s") {
+        cannon.angle = Math.max(cannon.angle - 2, cannon.minAngle);
+    }
+});
+
+canvas.addEventListener("mousedown", () => {
+    if (projectile.x === cannon.x) {
+        cannon.charging = true;
+    }
+});
+
+canvas.addEventListener("mouseup", () => {
+    if (cannon.charging) {
+        fireCannon();
+    }
+    cannon.charging = false;
+    cannon.power = 0;
+});
+
+function resetGame() {
+    projectile.x = cannon.x;
+    projectile.y = cannon.y;
+    document.getElementById("w-score").style.visibility = 'hidden';
+    document.getElementById("score").textContent = "SCORE: ";
+};
+
+function makeFull(type) {
+    oldW = document.getElementById(type).style.width;
+    document.getElementById(type).style.width = oldW + 500 + 'px';
+};
+
+function makeSmall(type) {
+    document.getElementById(type).style.width = null;
+};
+
+function closeDiv(type) {
+    document.getElementById(type).style.visibility = 'hidden';
+    resetGame();
+}
+
+function upgradeCannon() {
+    upgradeWindow = document.getElementById('w-popup');
+    upgradeWindow.style.visibility = 'visible';
+
+    document.getElementById("shop-item-1").src = '/images/cannons/better_base.png';
+};
+
+function buyItem(item, power, charge) {
+    document.getElementById(item).style.visibility = 'hidden';
+    cannon.maxPower += power;
+    cannon.chargeSpeed += charge;
+    cCosmetics.baseImage = '/images/cannons/' + item + '_base.png';
+    cCosmetics.barrelImage = '/images/cannons/' + item + '_barrel.png';
+};
